226c1d572dafef9c437035d5e6bc4dee72562c92
qo <72982727+qo@users.noreply.github.com>
2022-10-18 09:04:42 +0300

    Sprint 1

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
+app.listen(3000, () => {
+    console.log(
+        `
+        Сервер запущен на порту 3000.
+        http://localhost:3000/
+        `
+    );
+});
