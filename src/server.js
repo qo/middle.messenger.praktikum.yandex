8d163d3ecfef7f5708f63e8314abbebbe0629dd2
qo <72982727+qo@users.noreply.github.com>
2022-11-05 20:33:09 +0300

    Sprint 2 (минорные фиксы)

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(
