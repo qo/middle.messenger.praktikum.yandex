d5d0b15d24be1988dbc035039caa514da06f3287
qo <72982727+qo@users.noreply.github.com>
2022-12-08 18:45:35 +0300

    Фикс метода получения токена (на ручку /token/id нужно подавать id чата, а не id юзера)

--- src/utils/api/chats-api.ts
+++ src/utils/api/chats-api.ts
@@ -1,18 +1,12 @@
 import HTTP from "../http";
-import AuthAPIController from "../controllers/auth-api-controller";
 
 const chatsAPIInstance = new HTTP('https://ya-praktikum.tech/api/v2/chats');
 
 export default class ChatsAPI {
     public async getToken(): Promise<unknown> {
-        const controller = new AuthAPIController();
-        const user = await controller.getUser();
-        console.log(user);
-        // @ts-ignore
-        const id = JSON.parse(user).id;
-        console.log(id);
+        const chatID = 4136;
         return chatsAPIInstance.post(
-            `/token/${id}`
+            `/token/${chatID}`
         );
     }
 }
